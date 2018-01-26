a6a785fe2de7950ea9958304b1febdc7ef35c78a
Camilla Eldridge <k1116370@kingston.ac.uk>
2018-01-26 12:48:27 +0000

    added multf

---
+++ multif_to_singlef/multif_tosinglef_new.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+
+multif=sys.argv[1]
+fn = open(multif,"r").read()
+g=fn.split(">")[1:]
+
+for j, line in enumerate(g):
+    t=line.split()   
+    k="".join(line).split("\n")
+    header="".join(k[0]).rstrip()
+    seq="".join(k[1:]).rstrip()
+    f = open(header + "_" + str(j) + ".fasta",'w')
+    f.write(">" + header + "\n" +  seq)
+    f.close()
+
+
+#enumerate gives unique number of each sequence in case of identical ids..
