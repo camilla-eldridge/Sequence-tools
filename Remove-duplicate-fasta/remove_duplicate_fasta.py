5d5880da40105cd5ed88e5dce171f91955f27953
Camilla Eldridge <k1116370@kingston.ac.uk>
2018-01-30 20:16:58 +0000

    Update remove_duplicate_fasta.py

--- Remove-duplicate-fasta/remove_duplicate_fasta.py
+++ Remove-duplicate-fasta/remove_duplicate_fasta.py
@@ -3,21 +3,19 @@ import sys
 input_file=sys.argv[1]
 output_file=sys.argv[2]
 
-fasta_file=open(input_file, 'r')  #opens inputfile for reading
-fasta_lines=fasta_file.read().split('>')[0:]  #splits each sequence by header
-unique_sequences=open(output_file,'w')  #opens outputfile for writing
+fasta_file=open(input_file, 'r')  
+fasta_lines=fasta_file.read().split('>')[0:]  
+unique_sequences=open(output_file,'w')  
 
-def remove_complete_duplicates(fasta_lines): #start of definition of a new function with purpose: remove_duplicates from sequence list
-    outputlist=[] #creates an empty list to contain output/ uniquesequences
-    setofuniqsequence=set() #assigns a set for sequences -set can only contain uniquesequences 
-    for sequence in fasta_lines: #for loop - for sequence list in sequence list:
-	 if sequence not in setofuniqsequence: # If sequence has not been encountered yet i.e unique:
-           outputlist.append(sequence) # ... add it to list.
-           setofuniqsequence.add(sequence) #... add it to set.
+def remove_complete_duplicates(fasta_lines): 
+    outputlist=[] 
+    setofuniqsequence=set()
+    for sequence in fasta_lines: 
+	 if sequence not in setofuniqsequence: 
+           outputlist.append(sequence)
+           setofuniqsequence.add(sequence)
     return outputlist
 
-result=remove_complete_duplicates(fasta_lines) # Remove duplicates from the list and defines as 'result'
-unique_sequences.write('>'.join(result)) #'>' replaces lost > symbols
-unique_sequences.close() #closes output file
-
-#Usage: python script_name  fasta_file   output_fasta_file
+result=remove_complete_duplicates(fasta_lines) 
+unique_sequences.write('>'.join(result)) 
+unique_sequences.close() 
