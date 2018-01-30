0ef66d877b510140285ecb59d621300dcdcb3705
Camilla Eldridge <k1116370@kingston.ac.uk>
2018-01-30 21:43:19 +0000

    Update reverse_comp.py

--- reverse_complement/reverse_comp.py
+++ reverse_complement/reverse_comp.py
@@ -1,7 +1,6 @@
 import sys
 
 input_seq=sys.argv[1]
-
 sequence=open(input_seq, "r").read()
 
 a="".join(sequence.split("\n")[1:])
@@ -12,10 +11,3 @@ reverse=a[::-1].lower()
 reverse_complement=reverse.replace("a", "T").replace("t","A").replace("c","G").replace("g","C").upper()
 
 print header + "\n" + reverse_complement
-
-
-
-
-
-
-
