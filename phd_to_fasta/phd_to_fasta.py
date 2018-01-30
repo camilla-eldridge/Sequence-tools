ea58c0a0a83ba16d26f670169b7e27f220cb9244
Camilla Eldridge <k1116370@kingston.ac.uk>
2018-01-30 21:42:08 +0000

    Update phd_to_fasta.py

--- phd_to_fasta/phd_to_fasta.py
+++ phd_to_fasta/phd_to_fasta.py
@@ -1,13 +1,13 @@
 
 import sys
 p=sys.argv[1]
-ids=sys.argv[2]
+ID=sys.argv[2]
 
 pp=open(p, "r").read()
 
-def trim_read(PHD_file):
+def phd2fasta(PHD_file):
     splitphd=PHD_file.split("BEGIN_DNA")[1].split("END_DNA")[0]
     SEQ="".join(splitphd.split()[0::3])
-    return ">" + ids + "\n" + SEQ
+    return ">" + ID + "\n" + SEQ
 
-print trim_read(pp)
+print phd2fasta(pp)
