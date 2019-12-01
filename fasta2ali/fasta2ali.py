5d9301b455f3e9df19ffab71ac131bc8a1dfe710
camilla-eldridge <millyeldridge@hotmail.co.uk>
2019-12-01 14:29:32 +0000

    edited fasta2ali.py

--- fasta2ali/fasta2ali.py
+++ fasta2ali/fasta2ali.py
@@ -7,7 +7,7 @@ ffile=sys.argv[2]
 
 with open(ffile, "r") as fasta:
     fasta=fasta.readlines()
-    seq="".join(fasta[1:]).replace("\n", "") + "*"
+    seq="".join(fasta[1:]).replace("\n", "").upper() + "*"
     ali=">P1;" + str(iD) + "\n" + "sequence:" + str(iD) + ":::::::0.00: 0.00" + "\n" + seq
     print ali
 
