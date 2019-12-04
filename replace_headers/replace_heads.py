be1035289542192889184e9ee2bbda65b8d77671
camilla-eldridge <millyeldridge@hotmail.co.uk>
2019-12-04 20:17:38 +0000

    added replace_headers

---
+++ replace_headers/replace_heads.py
@@ -0,0 +1,27 @@
+import sys
+
+mf=sys.argv[1]
+new_heads=sys.argv[2]
+output=sys.argv[3]
+
+
+with open(new_heads, "r") as headers:
+    headers = headers.read().split()
+    
+    
+seq=[]
+
+with open(mf, "r") as f:
+    f=f.read()
+    f=f.split(">")[1:]
+    for line in f:
+        line=line.split(" ")
+        seq.append("\n".join("".join(line).split("\n")[1:]))
+        
+
+final = ["\n".join(a) for a in zip(headers, seq)]
+
+out = open(output, "w")
+out.write("".join(final))
+
+        
