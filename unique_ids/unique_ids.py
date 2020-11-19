dc39e579590f02b6e95a625fcc39c8e459133b69
Camilla Eldridge <k1116370@kingston.ac.uk>
2020-11-19 11:27:55 +0000

    added unique_ids.py

---
+++ unique_ids/unique_ids.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+
+multif=sys.argv[1]
+outf=sys.argv[2]
+
+fn=open(multif,"r").read()
+g=fn.split(">")[1:]
+out=open(outf, "w")
+
+saved=""
+for j, line in enumerate(g):
+    id=j+1
+    t=line.split()
+    k="".join(line).split("\n")
+    seq="".join(k[1:]).rstrip()
+    saved=saved + ">" + str(id) + "\n" + seq + "\n"
+out.write(saved)
+
