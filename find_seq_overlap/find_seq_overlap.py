4b2b99a97089ba2e808ebf11a9b999f2e5f25884
Camilla Eldridge <k1116370@kingston.ac.uk>
2022-02-03 21:51:53 +0000

    Update find_seq_overlap.py

--- find_seq_overlap/find_seq_overlap.py
+++ find_seq_overlap/find_seq_overlap.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+"""
+@author: camilla eldridge
+"""
 
 import sys
 import re
@@ -15,34 +18,24 @@ positions=open(out, "w")
 a="".join(sequence).split("\n")[1] 
 
 
-
 pos=""
 
 with open(epi) as epitopes:
-    
     epitopes = epitopes.readlines()
-    
     for line in epitopes:
-        
+    
         """ Remove epitope headers """
-        
         e=("".join(epitopes).split("\n")[1::2])
         
-      
         for i in e:
             
                 """ Find epitope in sequence (overlapping or not) """
-                
                 matches=re.finditer(r'(?=(%s))' % re.escape(i),a) 
                 
-                
                 """Get start:stop positions for epitopes and corresponding sequence"""
-                
                 pos=pos  +  str([('%01d %01d %s' % (m.start(1), m.end(1), m.group(1))) for m in matches]) + "\n"   
         
         break
-
-
 
 k=pos.replace("[]", "").replace("[", "").replace("]", "").replace("'", "")
 
