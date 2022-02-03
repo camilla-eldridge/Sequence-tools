b52f788d637628bc62106a964bf2220de28d7d88
Camilla Eldridge <k1116370@kingston.ac.uk>
2022-02-03 21:44:48 +0000

    updated for py3

--- fasta2ali/fasta2ali.py
+++ fasta2ali/fasta2ali.py
@@ -9,5 +9,5 @@ with open(ffile, "r") as fasta:
     fasta=fasta.readlines()
     seq="".join(fasta[1:]).replace("\n", "").upper() + "*"
     ali=">P1;" + str(iD) + "\n" + "sequence:" + str(iD) + ":::::::0.00: 0.00" + "\n" + seq
-    print ali
+    print(ali)
 
