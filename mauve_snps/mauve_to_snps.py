fdd4ef787cad2e20530d44c8461375d728c5effb
camilla-eldridge <camilla.jl.eldridge@gmail.com>
2023-08-18 17:44:51 -0400

    added gene product info to output and general update

--- mauve_snps/mauve_to_snps.py
+++ mauve_snps/mauve_to_snps.py
@@ -21,18 +21,22 @@ first_gbk=sys.argv[2]
     3. Genbank file generated from prokka 1.12 in 2017 '''
 
 
+
+
+''' get node ids and snp pos for sequence 1 only'''
+
 contig_pos=[]
 
 with open(mauve_SNP_output, "r") as snps:      
     each_snp=filter(None, snps.read().split("\n")[1:])
     for j in each_snp:
         j=j.split()
-        contig_pos.append(j[2] + "," +  j[3])
+        j12="_".join(str(j[1]).split("_")[0:2])
+        contig_pos.append(j12 + "," + j[2] + "," +  j[3])
         
 
+''' Get set of node ids with snps in'''
 
-
-''' Get set of node ids with snps in '''
 snp_node_ids=set()
 
 for node in contig_pos:
@@ -41,43 +45,102 @@ for node in contig_pos:
 
 
 ''' Get gbk entries for snp nodes '''
+
 gbk_snp_entries=[]
 
 with open(first_gbk, "r") as gbk:
     
-    gbk1=gbk.read().split("LOCUS") # split by locus i.e label for contig id
+    # split by locus i.e label for contig id
+    
+    gbk1=gbk.read().split("LOCUS") 
     
     for entry in gbk1:
+        at=entry.split("\n")[0].split()
+        at2=",".join(at).split(",")[0]
+        
+        
         for node_id in snp_node_ids:
-            if str(node_id) in entry:
+            if str(node_id) == str(at2):
                 gbk_snp_entries.append(entry)
                 
                 
-    
-''' Function for each contig entry '''
+            
+                
+
+''' Function to find annotated regions for each contig'''
+
 def get_annotations(x):
     contig_cds=[]
+
+    NODEid=x.split()[0]
     
-    NODE=x.split("\n")[0].split()[0]
-    contig_cds.append(NODE)
+    contig_cds.append(NODEid)
     y = x.split("ORIGIN")[0][1:]
+
+
     
-    for Q in y.split("\n"):             
+    for Q in y.split("\n"):
+        to_app=""
+
         ''' loop through entries, get range '''
         if "rRNA  " in Q  or " CDS " in Q  or  " tRNA " in Q:
-            contig_cds.append(Q.replace("complement", "").replace("..", " ").replace("(", "").replace(")", ""))
+            
+                to_app = to_app + Q.replace("complement", "").replace("..", " ").replace("(", "").replace(")", "")
+        else:
+            pass
+        
+            
+        if "/product=" in Q:
+            
+            to_app = to_app + Q.strip().replace(" ", "_").replace("/", "")
+            
+        else:
+            pass
+        
+        contig_cds.append(to_app)
+        
+    contig_cds_strp = [item.strip() for item in contig_cds if item.strip() != '']
     
-    return(contig_cds)
+    return(contig_cds_strp)
     
+
+
     
-    
-''' Get range for annotated entry in each contig '''
+''' Get annotated entry in each contig '''
+
 all_snp_conts=[]
 for C in gbk_snp_entries:
     all_snp_conts.append(get_annotations(C))
-       
-          
+    
+    
+
+''' some nodes dont have annotation, contaminants? Print out, remove, replace annotation features to get just pos'''
+
+all_snp_conts_replaced=[]
+
+for E in all_snp_conts:  
+    
+    nod_id = str(E).split()[0]
+    
+    if len(E) < 2:  # if no annotated region in the node, print the node id  
+    
+        print("".join(E) + ": SNP here but no annotations found!")   
+        
+    else:        
+        
+        ''' reformat the string, theres probs a better way...''' 
+        # replace annotation info with node id (we dont need it)
+        
+        E=str(E).replace("CDS ", nod_id).replace("tRNA ", nod_id).replace("rRNA ", nod_id).replace("'", "").replace(",", "").replace("[", "").replace("]", "").replace("product=", "").replace('"', "")
+        
+        # remove additional node id info at start
+        all_snp_conts_replaced.append([r for r in E.split()[1:]])
+    
+    
+
+
 ''' remove duplicate node id entries from snp list '''
+
 w=[]
 
 for i in contig_pos:
@@ -94,58 +157,60 @@ for i in contig_pos:
     w.append(nod_pos)
     
     
+    
 ''' get all snp positions for each node '''
+
 snp_node_positions = list(filter(None, [k.split() for k in " ".join(w).split("split_me")]))
 
 
-''' some nodes dont have annotation, contaminants? Print out, remove, replace annotation features to get just pos'''
-all_snp_conts_replaced=[]
-for E in all_snp_conts:    
-    if len(E) < 2:  # if no annotated region in the node, print the node id    
-        print("".join(E) + ": SNP here but no annotations found!")   
-    else:        
-        ''' reformat the string, theres probs a better way...'''        
-        E=str(E).replace("CDS", "").replace("tRNA", "").replace("rRNA", "").replace("'", "").replace(",", "").replace("[", "").replace("]", "")
-        all_snp_conts_replaced.append([r for r in "".join(E).split()])
-        
-      
+
+''' input list of snp positions per node, and list of gbk coding regions '''
+
+snps_in_annot = {}
+
+for snp_val in snp_node_positions:
+    node = snp_val[0]
+    snps_in_annot[node] = []
     
-def check_range(range_list, snp_list, Node_ID):    
-     ''' check if snp pos is in the range of annotated feature '''     
-     snps_out=""     
-     range_pairs=[range_list[a:a+2] for a in range(0, len(range_list), 2)]
-     
-     for ra in range_pairs:  
-         for s in snp_list:    
-             
-             ''' if snp is in range          
-             return node id, snp and range '''
-             
-             if int(s) in range(int(ra[0]), int(ra[1])):        
-                 snps_out=snps_out + Node_ID + "," +  s + "," + ":".join(ra) + "\n"
-             else:
-                 pass
-     return(snps_out)
+    for triplet in all_snp_conts_replaced:
+        triplet_node = triplet[0]
+        
+        if triplet_node == node:
+            values = snp_val[1:]
+            for i in range(1, len(triplet), 4):
+                product = triplet[i + 2]
+                range_values = triplet[i:i + 2]
+                
+                for value in values:
+                    int_value = int(value)
+                    if int(range_values[0]) <= int_value <= int(range_values[1]):
+                        snps_in_annot[node].append({'product': product, 'value': value, 'range': range_values})
+                        
+                        # break the loop here to prevent duplicates
+                        break
+                        
+        else:
+            pass
+        
+        
+# Remove entries with empty key or values
+
+snps_in_annot = {k: v for k, v in snps_in_annot.items() if k and v}
+
+
+for node, entries in snps_in_annot.items():
+    print(f"Node: {node}")
+    for entry in entries:
+        product = entry['product']
+        value = entry['value']
+        value_range = entry['range']
+        print(f"Product: {product}")
+        print(f"Value: {value}")
+        print(f"Range: {', '.join(value_range)}")
+        print("-----")
+
+       
             
-        
-     
-''' check if snps are in annotated range ''' 
-for loc in all_snp_conts_replaced:
-     nodE=str(loc[0].replace("[", ""))
-     
-     ''' for each snp found in each node ''' 
-     for F in snp_node_positions: 
-         
-         ''' if node id from snp contigs is the same as node id in the snp list '''     
-         if str(F[0]) == nodE:
-             
-             ''' check if snps ina given node are in the cds/rRNA/tRNA range '''         
-             each_check=check_range(loc[1:], F[1:], nodE)         
-             print(each_check)
-         
-         
-    
-    
 
 ''' Notes:
     
